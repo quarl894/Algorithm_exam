58dd8cf50f34d661eb0dae8faf3383d58b8cc5f8
quarl894 <quarl894@gmail.com>
2018-01-30 17:56:57 +0900

    beakjoon 1699

---
+++ Algo/src/backjune/ex_1699.java
@@ -0,0 +1,28 @@
+package backjune;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class ex_1699 {
+	static int count =0;
+	public static void main(String[] args) throws Exception{
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+		int dp[] = new int[N+1];
+		
+		//초기화(다 1^2일때 가정)
+		for(int i=1; i<=N; i++){
+			dp[i] = i;
+		}
+		
+		// 1을 더하는건 j*j 제곱수가 추가되는 의미.
+		for(int i=2; i<=N; i++){
+			for(int j=2; j*j<=i; j++){
+				dp[i] = Math.min(dp[i], dp[i-j*j]+1);
+			}
+		}
+		System.out.println(dp[N]);
+		br.close();
+		
+	}
+}
