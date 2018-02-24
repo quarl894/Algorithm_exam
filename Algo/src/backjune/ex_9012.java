f0322284d65346eeab601fe42b0d4a04c188d4fc
quarl894 <quarl894@gmail.com>
2018-02-24 16:28:34 +0900

    beakjoon 9012

--- Algo/src/backjune/ex_9012.java
+++ Algo/src/backjune/ex_9012.java
@@ -5,20 +5,26 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 //괄호
+// 스택으로 괄호 확인
 public class ex_9012 {
 	public static void main(String[] args) throws Exception{
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int N = Integer.parseInt(br.readLine());
 		Stack<Character> stack = new Stack<>();
 		String[] input = new String[N];
+		
 		for(int i=0; i<N; i++){
 			input[i] = br.readLine();
 		}
+		
 		for(int i=0; i<input.length; i++){
 			for(int j=0; j<input[i].length(); j++){
+				//'('이면 stack push함.
 				if(input[i].charAt(j)=='('){
 					stack.push(input[i].charAt(j));
+				// ')'이 들어오면 pop을 함.
 				}else if(input[i].charAt(j)==')'){
+					// 이미 stack이 비어있을 수 있으니 예외처리함.
 					if(stack.isEmpty()){
 						stack.push('x');
 						break;
